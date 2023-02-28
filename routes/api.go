38e3af017c79711445529b0f657ef38ddd243515
shehongbo <244117623@qq.com>
2023-02-28 18:03:27 +0800

    Email 是否已注册接口

--- routes/api.go
+++ routes/api.go
@@ -16,6 +16,8 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			suc := new(auth.SignupController)
 			//判断手机是否已注册
 			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+			//判断Email是否已注册
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 		}
 	}
 }
