5052e64b9d295dab27b54e4d1e3d695464619f16
shehongbo <244117623@qq.com>
2023-03-03 18:00:19 +0800

    发送短信验证码

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"gohub/bootstrap"
 	btsConfig "gohub/config"
 	"gohub/pkg/config"
-	"gohub/pkg/verifycode"
 )
 
 func init() {
@@ -44,8 +43,6 @@ func main() {
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(router)
-
-	verifycode.NewVerifyCode().SendSMS("00013800138001")
 
 	// 运行服务
 	err := router.Run(":" + config.Get("app.port"))
