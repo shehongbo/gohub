b6bf20601087c21ce889f39e15875a0909376280
shehongbo <244117623@qq.com>
2023-03-16 15:15:37 +0800

    话题模型和迁移

---
+++ database/migrations/2023_23_16_150520_add_topics_table.go
@@ -0,0 +1,44 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type User struct {
+		models.BaseModel
+	}
+
+	type Category struct {
+		models.BaseModel
+	}
+	type Topic struct {
+		models.BaseModel
+
+		Title      string `gorm:"type:varchar(255);not null;index"`
+		Body       string `gorm:"type:longtext;not null"`
+		UserID     string `gorm:"type:bigint;index;not null"`
+		CategoryID string `gorm:"type:bigint;index;not null"`
+
+		// 会创建 user_id 和 category_id 外键的约束
+		User     User
+		Category Category
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Topic{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Topic{})
+	}
+
+	migrate.Add("2023_23_16_150520_add_topics_table", up, down)
+}
