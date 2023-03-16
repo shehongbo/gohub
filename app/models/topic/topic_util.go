db4882f35ce768595d76f5ecdd31cf597406ed78
shehongbo <244117623@qq.com>
2023-03-16 16:32:52 +0800

    显示话题

--- app/models/topic/topic_util.go
+++ app/models/topic/topic_util.go
@@ -1,26 +1,27 @@
 package topic
 
 import (
-    "gohub/pkg/database"
+	"gohub/pkg/database"
+	"gorm.io/gorm/clause"
 )
 
 func Get(idstr string) (topic Topic) {
-    database.DB.Where("id", idstr).First(&topic)
-    return
+	database.DB.Preload(clause.Associations).Where("id", idstr).First(&topic) //附带 user 和 category 关联信息
+	return
 }
 
 func GetBy(field, value string) (topic Topic) {
-    database.DB.Where("? = ?", field, value).First(&topic)
-    return
+	database.DB.Where("? = ?", field, value).First(&topic)
+	return
 }
 
 func All() (topics []Topic) {
-    database.DB.Find(&topics)
-    return
+	database.DB.Find(&topics)
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Topic{}).Where("? = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Topic{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
 }
