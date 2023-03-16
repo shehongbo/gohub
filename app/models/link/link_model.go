b2f8de214ea0c98328f26eeaffd341a0ee85b085
shehongbo <244117623@qq.com>
2023-03-16 16:43:41 +0800

    友情链接模型和迁移

---
+++ app/models/link/link_model.go
@@ -0,0 +1,44 @@
+// Package link 模型
+package link
+
+import (
+	"gohub/app/models"
+	"gohub/pkg/app"
+	"gohub/pkg/database"
+	"gohub/pkg/paginator"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Link struct {
+	models.BaseModel
+	Name string `json:"name,omitempty"`
+	URL  string `json:"url,omitempty"`
+	models.CommonTimestampsField
+}
+
+func (link *Link) Create() {
+	database.DB.Create(&link)
+}
+
+func (link *Link) Save() (rowsAffected int64) {
+	result := database.DB.Save(&link)
+	return result.RowsAffected
+}
+
+func (link *Link) Delete() (rowsAffected int64) {
+	result := database.DB.Delete(&link)
+	return result.RowsAffected
+}
+
+// Paginate 分页内容
+func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
+}
---
+++ database/migrations/2023_23_16_164155_add_links_table.go
@@ -0,0 +1,31 @@
+package migrations
+
+import (
+	"database/sql"
+	"gohub/app/models"
+	"gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URl  string `gorm:"type:varchar(255);index;default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2023_23_16_164155_add_links_table", up, down)
+}
